79d064b1516727bcc7ce26fdcd64ddcd41134c61
sanjeevhadapad <sanjeevhadapad10@gmail.com>
2025-10-23 17:16:14 +0530

    save to withdra_counter branch

--- bank/amount.py
+++ bank/amount.py
@@ -6,3 +6,10 @@ new_balance = initial_balance + deposit_amount
 print(f"Initial Balance: ₹{initial_balance}")
 print(f"Deposit: ₹{deposit_amount}")
 print(f"New Balance after deposit: ₹{new_balance}")
+withdraw_amount = float(input("Enter Withdraw Amount: ₹"))
+
+final_balance = balance_after_deposit - withdraw_amount
+
+# Display final result
+print(f"Withdraw: ₹{withdraw_amount}")
+print(f"Final Balance: ₹{final_balance}")
