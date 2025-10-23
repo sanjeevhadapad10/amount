8820731e6d3a1a298542e0fcd00ac05eef3d442a
sanjeevhadapad <sanjeevhadapad10@gmail.com>
2025-10-23 14:12:02 +0530

    save

---
+++ ptrr.py
@@ -0,0 +1,8 @@
+
+P = float(input("Enter Principal Amount (₹): "))
+R = float(input("Enter Rate of Interest (%): "))
+T = float(input("Enter Time (in years): "))
+
+SI = (P * R * T) / 100
+
+print("Simple Interest: ₹", SI)
