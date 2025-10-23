ab4276d23c8aa1c67dd1a33ea6433c5222d0f07a
sanjeevhadapad <sanjeevhadapad10@gmail.com>
2025-10-23 17:42:21 +0530

    save to withdraw branch

--- deposit/balance.py
+++ deposit/balance.py
@@ -11,3 +11,7 @@ new_balance = initial_balance + deposit_amount
 print(f"Initial Balance: ₹{initial_balance}")
 print(f"Deposit: ₹{deposit_amount}")
 print(f"New Balance after deposit: ₹{new_balance}")
+withdraw_amount = float(input("Enter Withdraw Amount: ₹"))
+
+# Calculate final balance
+final_balance = balance_after_deposit - withdraw_amount
